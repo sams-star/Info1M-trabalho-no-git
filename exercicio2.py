38ae98878e7d77b3ba0f22d75ec77c114854035b
sams-star <samelazuck7@gmail.com>
2025-09-17 10:18:58 -0300

    mudança 3

--- exercicio2.py
+++ exercicio2.py
@@ -1,6 +1,6 @@
 1 = int(input("digite um numero :"))
 2 = int(input("digite um numero dois:"))
 
-a = 1+2
+soma = 1+2
 
-print(f'Olá, {a}')
+print(soma)
---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+1 = int(input("média :"))
+2 = int(input("média 2:"))
+3 = int(input("média 3:"))
+4 = int(input("média 4 :"))
+
+a = 1 + 2 + 3 + 4 /4
+
+print(a)
